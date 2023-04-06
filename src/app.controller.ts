c2de987179f334f96c1fae5ae6d791558fabbbd7
Dieguidev <diegogaraycullas@gmail.com>
2023-04-06 15:42:33 -0500

    identificando parametros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,26 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Bienvenido a mi Api';
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'con /sas/';
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return `product ${id}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('id') id: string, @Param('productId') productId: string) {
+    return `product ${productId} and ${id}`;
   }
 }
